b71140a825f0bae44a082c56334de496ab2197c5
Adiiieee <adiparmar2982@gmail.com>
2024-04-16 13:54:47 +0530

    scaffold ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,27 @@
+import Image from "next/image";
+
+const mockUrls = [
+  "https://utfs.io/f/d120e44f-be44-4479-89f9-09e67e3d20d8-gawbxk.jpg",
+  "https://utfs.io/f/c7ddb322-66c2-4916-acef-cfe8794b7466-eqaer4.jpg",
+  "https://utfs.io/f/c018a167-5136-465a-8178-7bdc6d7ea3bd-edw8i.jpg",
+  "https://utfs.io/f/eda89908-823b-4d45-b05f-a0c4f1e43bad-htk5r4.jpg",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <p>This is my app</p>
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {mockImages.map((image) => (
+          <div key={image.id} className="w-48">
+            <Image src={image.url} alt="image" width={200} height={200} />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
